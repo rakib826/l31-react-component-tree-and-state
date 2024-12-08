5b1f498179da0e4575638bb554a2e89489891c14
Kazi Md. Rakibul Hassan <2220936@iub.edu.bd>
2024-12-08 19:57:24 +0600

    addeed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,30 @@ function App() {
 
   return (
     <>
-    <h1>Hello World</h1>
+    <div>
+      <nav>
+        <div>Brand</div>
+        <div>
+          <ul>
+            <li>One</li>
+            <li>Two</li>
+            <li>Three</li>
+          </ul>
+        </div>
+      </nav>
+      <main>
+        <h1>Main Section</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium porro voluptate recusandae eligendi consequuntur cupiditate reiciendis sit, enim nisi ea ipsa animi aliquid veniam. Quas at dolorem animi praesentium aut quasi, nobis excepturi molestiae provident. Rem reprehenderit, a autem voluptates facere accusamus esse beatae repellat eveniet dolor nam magnam excepturi.</p>
+      </main>
+      <footer>
+        <h3>Footer</h3>
+        <ul>
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </ul>
+      </footer>
+    </div>
     </>
   )
 }
